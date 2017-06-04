332b1f453f8764057007b9f72f0040b74de72148
Jim Harris <jharris99@gatech.edu>
2017-06-04 11:21:06 -0400

    hello world!

---
+++ suds.py
@@ -0,0 +1,13 @@
+"""Super Duper Spork
+This module is for performing a search on wikipedia for an article, and printing
+a summary of it.
+
+Depends on bs4 and nltk.
+
+Runs on python 3.
+"""
+
+if __name__ == '__main__':
+    print('Staring program')
+else:
+    print('Not starting program')
