d14c297cdc09f72d352be12092b31ac34b78a584
Jim Harris <jharris99@gatech.edu>
2017-06-10 18:16:38 -0400

    more pretty printing

--- super_duper_spork/super_duper_spork.py
+++ super_duper_spork/super_duper_spork.py
@@ -11,34 +11,35 @@ import wiki_summarize
 
 def print80(text):
     words = text.split(' ')
-    line_length = 0
-    line = ''
+    line_length = 2
+    line = '| '
     for word in words:
-        if line_length + len(word) + 1 <= 80:
+        if line_length + len(word) + 1 <= 79:
             line_length += len(word) + 1
             line += word + " "
         else:
-            print(line)
-            line = word + ' '
-            line_length = len(word) + 1
+            print(line + ' ' * (79 - line_length) + '|')
+            line = '| ' + word + ' '
+            line_length = len(word) + 3
     if line != '':
-        print(line)
+        print(line + ' ' * (79 - line_length) + '|')
 
 def print_article_summary(markup):
     sections = wiki_summarize.extract_sections(markup)
     for title, text in sections:
-        print('+' + '-' * 79)
-        print('| ' + title)
-        print('+' + '-' * 79)
+        print('+' + '-' * 78 + '+')
+        print80(title)
+        print('+' + '-' * 78 + '+')
         print80(text)
+    print('+' + '-' * 78 + '+')
 
 def print_search_suggestions(markup):
-    print('+' + '-' * 79)
-    print("| No article found! Here are some suggestions:")
-    print('+' + '-' * 79)
+    print('+' + '-' * 78 + '+')
+    print80("No article found! Here are some suggestions:")
+    print('+' + '-' * 78 + '+')
     for suggestion in wiki_summarize.get_search_results(markup):
         print("| - " + suggestion)
-    print('+' + '-' * 79)
+    print('+' + '-' * 78 + '+')
 
 if __name__ == '__main__':
     if len(argv) != 2:
