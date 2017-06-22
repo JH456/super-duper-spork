6da9b48ccde5b2517d3262363f68a27401182f1c
Jim Harris <jharris99@gatech.edu>
2017-06-21 21:37:10 -0400

    changed print80 to prettify_text

--- super_duper_spork/super_duper_spork.py
+++ super_duper_spork/super_duper_spork.py
@@ -9,44 +9,56 @@ Usage:
 from sys import argv
 import wiki_summarize
 
-def print80(text):
+def prettify_text(text, max_line_length):
+    """
+    Adds line endings to a string so each line is of 80 characters each at most.
+
+    Will also add in pipes to pretty print the output.
+
+    Keyword arguments:
+    text -- a string of words separated by spaces to pretty print.
+    max_line_length -- int
+
+    """
     words = text.split(' ')
     line_length = 2
     line = '| '
+    pretty_text = ''
     for word in words:
-        if line_length + len(word) + 1 <= 79:
+        if line_length + len(word) + 2 <= max_line_length:
             line_length += len(word) + 1
             line += word + " "
         else:
-            print(line + ' ' * (79 - line_length) + '|')
+            pretty_text += line + ' ' * (max_line_length - 1 - line_length) + '|' + '\n'
             line = '| ' + word + ' '
             line_length = len(word) + 3
     if line != '':
-        print(line + ' ' * (79 - line_length) + '|')
+        pretty_text += (line + ' ' * (max_line_length - 1 - line_length) + '|')
+    return pretty_text
 
 def print_article_summary(markup):
     sections = wiki_summarize.extract_sections(markup)
     for title, text in sections:
         print('+' + '-' * 78 + '+')
-        print80(title)
+        print(prettify_text(title, 80))
         print('+' + '-' * 78 + '+')
-        print80(wiki_summarize.summarize_text(text))
+        print(prettify_text(wiki_summarize.summarize_text(text), 80))
     print('+' + '-' * 78 + '+')
 
 def print_disambiguation(markup):
     print('+' + '-' * 78 + '+')
-    print80("Your search may refer to:")
+    print(prettify_text("Your search may refer to:", 80))
     print('+' + '-' * 78 + '+')
     for suggestion in wiki_summarize.get_disambiguation_results(markup):
-        print80("- " + suggestion)
+        print(prettify_text("- " + suggestion, 80))
     print('+' + '-' * 78 + '+')
 
 def print_search_suggestions(markup):
     print('+' + '-' * 78 + '+')
-    print80("No article found! Here are some suggestions:")
+    print(prettify_text("No article found! Here are some suggestions:", 80))
     print('+' + '-' * 78 + '+')
     for suggestion in wiki_summarize.get_search_results(markup):
-        print80("- " + suggestion)
+        print(prettify_text("- " + suggestion, 80))
     print('+' + '-' * 78 + '+')
 
 if __name__ == '__main__':
